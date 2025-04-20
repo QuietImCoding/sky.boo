5f893eb71d43c9733e9a96be3582b73592d97fcb
QuietImCoding <dmonte3210@gmail.com>
2025-04-20 00:17:58 -0700

    Added bowl Hat for tasha

--- src/routes/avatar/LayersConfig.ts
+++ src/routes/avatar/LayersConfig.ts
@@ -1,7 +1,7 @@
 export const layers = [
     { name: "backdrop", count: 11, layer: "bottom" },
     { name: "ghostie", count: 9, layer: "middle" },
-    { name: "hat", count: 4, layer: "top" },
+    { name: "hat", count: 5, layer: "top" },
     { name: "glasses", count: 4, layer: "top" },
     { name: "photobomb", count: 3, layer: "top"}
 ];
