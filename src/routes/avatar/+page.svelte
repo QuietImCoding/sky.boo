be76e9756780255f60a48f82b68e0fee10d1f2a4
QuietImCoding <dmonte3210@gmail.com>
2025-04-19 19:23:22 -0700

    added full avatar skeleton, working on modularizing

--- src/routes/avatar/+page.svelte
+++ src/routes/avatar/+page.svelte
@@ -1,25 +1,29 @@
-<script module lang='ts'>
+<script module lang="ts">
   import AvatarStack from "./AvatarStack.svelte";
   import ControlRow from "./ControlRow.svelte";
   import "/src/lib/styles/avatar.css";
 
-  interface Avatar { backdrop: number, ghostie: number, hat: number, glasses: number }
-  export const avatarState : Avatar = $state({
-    backdrop: 0, 
-    ghostie: 0, 
-    hat: 0, 
-    glasses: 0
+  interface Avatar {
+    backdrop: number;
+    ghostie: number;
+    hat: number;
+    glasses: number;
+  }
+  export const avatarState: Avatar = $state({
+    backdrop: 0,
+    ghostie: 0,
+    hat: 0,
+    glasses: 0,
   });
-  
 </script>
 
 <title>AVATAR</title>
 
 <p>Customize your avatar</p>
 <div id="avatar">
-  <AvatarStack/>
+  <AvatarStack />
   <div id="controls">
-    <ControlRow target="backdrop"/>
+    <ControlRow target="backdrop" />
     <ControlRow target="ghostie" />
     <ControlRow target="hat" />
     <ControlRow target="glasses" />
@@ -27,3 +31,18 @@
 </div>
 
 <a href="/avatar/submissions">Submit your own ghost pieces</a>
+
+<style>
+  a {
+    background-color: magenta;
+    font-size: 2em;
+    padding: 5px;
+    font-family: "Gaegu Bold";
+    margin: 10px;
+  }
+
+  a:hover {
+    background-color: pink;
+    transition: 0.1s;
+  }
+</style>
--- src/routes/avatar/AvatarStack.svelte
+++ src/routes/avatar/AvatarStack.svelte
@@ -2,16 +2,25 @@
 
     import {avatarState}  from "./+page.svelte";
 
+    let layers = [
+        { name: "backdrop", count: 5, layer: "bottom" },
+        { name: "ghostie", count: 7, layer: "middle" },
+        { name: "hat", count: 3, layer: "top" },
+        { name: "glasses", count: 4, layer: "top" }
+    ];
+
     let backdropURL = $derived('/avatars/background/' + avatarState.backdrop % 5 + '.png');
     let ghostieURL = $derived('/avatars/ghostie/' + avatarState.ghostie % 7 + '.png');
-    let hatURL = $derived('/avatars/hat/' + avatarState.hat % 2 + '.png');
-    let glassesURL = $derived('/avatars/glasses/' + avatarState.glasses % 2 + '.png');
+
+    //OPTIONAL
+    let hatURL = $derived('/avatars/hat/' + avatarState.hat % 3 + '.png');
+    let glassesURL = $derived('/avatars/glasses/' + avatarState.glasses % 4 + '.png');
 
 </script>
 
 <div id="stack">
-    <img src={backdropURL} alt="" id="backdrop">
-    <img src={ghostieURL} alt="" id="ghostie"/>
-    <img src={hatURL} alt="" id="hat">
-    <img src={glassesURL} alt="" id="glasses">
+    <img src={backdropURL} alt="" id="backdrop" class="stacked bottom">
+    <img src={ghostieURL} alt="" id="ghostie" class="stacked middle"/>
+    <img src={hatURL} alt="" id="hat" class="stacked top">
+    <img src={glassesURL} alt="" id="glasses" class="stacked top">
 </div>
